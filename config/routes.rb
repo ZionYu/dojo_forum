0189068318fb798333619c804bf930d6e8e4610b
Zion <bone2057@gmail.com>
2018-10-18 05:23:21 +0800

    add index link on brand & set up routes for categroies

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
   resources :posts
+  resources :categories, only: [:show]
 
   namespace :admin do
     root "categories#index"
