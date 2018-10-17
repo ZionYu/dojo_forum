54ed494af381a1d7f1aa48ad971f9cfeff353e41
Zion <bone2057@gmail.com>
2018-10-17 20:01:27 +0800

    admin  can see all users in index page & update their authorizaiton

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,25 @@
 class Admin::UsersController < Admin::BaseController
+
+  def index
+    @users = User.all
+    
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(role: params[:role])
+    if @user.save
+      redirect_to admin_users_path
+      flash[:notice] = "Authorization update succeeded"
+    else
+      flash[:alert] = "Not allow"
+      render :index
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permint(:name, :eamil, :role)
+  end
+
 end
