835eec4f9e0a4444aae5a0e184ab7464045e54c1
Zion <bone2057@gmail.com>
2018-10-22 02:00:52 +0800

    change column default & setup relations for friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,29 @@ class User < ApplicationRecord
   has_many :collects, dependent: :destroy
   has_many :collected_posts, through: :collects, source: :post
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships, source: :user
+
   has_many :vieweds, dependent: :restrict_with_error
   has_many :viewed_posts, through: :vieweds, source: :post
   
   def admin?
     self.role == "Admin"
+  end
+
+  def friend?(user)
+    friendship_record = Friendship.where(user_id: user.id, friend_id: self.id).or(Friendship.where(user_id: self.id, friend_id: user.id))
+    if friendship_record.any?
+      # 有好友記錄
+      if friendship_record.first.status == "accept"
+        return "friend"
+      else
+        return "exist"
+      end
+    else
+      # 沒有好友記錄
+      return "none"
+    end
   end
   
 end
