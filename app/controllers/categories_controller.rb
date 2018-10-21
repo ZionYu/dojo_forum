0a91172470c4df5e7d94078400398fa312eae3fd
Zion <bone2057@gmail.com>
2018-10-22 06:12:37 +0800

    users can choose to sort posts in a specific category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts
+    @ransack = @category.posts.where(status: "published").ransack(params[:q])
+    @posts = @ransack.result(distinct: true).page(params[:page]).per(20)
   end
 end
